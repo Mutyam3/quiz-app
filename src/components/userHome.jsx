0cc03a29bdb1882eb9b06d99f4c6a9581db92f58
Mutyam3 <bhargavreddymutyam@gmail.com>
2025-01-12 15:02:30 +0530

    commit

--- src/components/userHome.jsx
+++ src/components/userHome.jsx
@@ -1,81 +1,80 @@
-import React from 'react'
-import { useParams ,Link } from 'react-router-dom'
-import axios from 'axios'
-import UserQuiz from './userQuiz'
 
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import UserQuiz from './userQuiz';
 
-function UserHome(){
+function UserHome() {
+  const { id } = useParams();
+  const [step, setStep] = React.useState(1);
+  const [userData, setUserData] = React.useState({});
+  const [quizData, setQuizData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [userQuiz, setUserQuiz] = React.useState({})
 
-    const {id} = useParams()
-    const [step, setStep] = React.useState(1)
-    const [userData, setUserData] = React.useState({})
-    const [quizData, setQuizData] = React.useState([])
-
-    React.useEffect(()=>{
-
-      axios.get(`http://localhost:5000/users/${id}`).then((res)=>{
-
-        console.log(res.data)
-        setUserData(res.data)
-
-      }).catch((err)=>{
-
-           console.log(err)
-
+  React.useEffect(() => {
+    axios
+      .get(`http://localhost:5000/users/${id}`)
+      .then((res) => {
+        setUserData(res.data);
+        setLoading(false);
       })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+        alert('Failed to fetch user data.');
+      });
+  }, []);
 
-         axios.get('http://localhost:5000/allQuizes').then((res)=>{
+  React.useEffect(() => {
+    if (userData?.quizId?.length) {
+      userData.quizId.forEach((el) => {
+        axios
+          .get(`http://localhost:5000/allQuizes/${el}`)
+          .then((res) => setQuizData((prev) => [...prev, res.data]))
+          .catch((err) => console.error(err));
+      });
+    }
+  }, [userData]);
 
-            console.log(res.data)
-             setQuizData(res.data)
-         }).catch((err)=>{
-              console.log(err)
-         })
+  const userQuizFn = (quiz) => {
+    console.log(quiz)
+    setUserQuiz(quiz)
+    setStep(2);
+  };
 
+  if (loading) return <div>Loading...</div>;
 
-    },[])
+  return (
+    <section className="p-5">
+      {step === 1 && (
+        <>
+          <div className="m-5 p-5 border border-dark text-center">
+            <h1>HI, {userData.name}</h1>
+          </div>
 
-    
-  
-    return (
-        <section className='p-5'>
+          <h5 className="m-5">Quizzes</h5>
+          <div className="border border-dark rounded-3 d-flex flex-wrap m-5 p-4">
+            {quizData.length > 0 ? (
+              quizData.map((quiz) => (
+                <section
+                  key={quiz.id}
+                  onClick={() => userQuizFn(quiz)}
+                  className="d-flex border border-dark p-5 m-3 w-75"
+                >
+                  <h4 className="text-center">{quiz.quizName}</h4>
+                </section>
+              ))
+            ) : (
+              <p>No quizzes available.</p>
+            )}
+          </div>
+        </>
+      )}
 
-            {
-              step == 1  && <>
-
-               <div className='m-5 p-5 border border-dark text-center'>
-                        <h1>HI, {userData.name}</h1>
-               </div>
-
-
-                <h5 className='m-5 '>Quizes</h5>
-               <div className='border border-dark rounded-3 d-flex m-5 p-4'>
-                   {
-                       quizData?.map((quiz)=>{
-                            return <Link to={`/userQuiz/${quiz.id}`} className='border border-dark w-25 p-5 rounded-3' style={{textDecoration:'none', color:'black'}}>
-                            <section>
-                                 <h4>{quiz.quizName}</h4>
-                                 
-                            </section>
-                            </Link>
-                       })
-                   }
-               </div>
-                </>
-            }
-               
-
-            {
-
-                step== 2 && 
-                <>
-                    <UserQuiz userData = {userData}/>
-                </>
-            }
-
-  
-        </section>
-    )
+      {step === 2 && <UserQuiz userQuiz = {userQuiz} setStep={setStep}/>}
+    </section>
+  );
 }
 
-export default UserHome
+export default UserHome;
--- src/components/userQuiz.jsx
+++ src/components/userQuiz.jsx
@@ -1,19 +1,72 @@
+import React from 'react';
 
-import React from 'react'
-import axios from 'axios'
-import {  useNavigate, useParams} from 'react-router-dom'
+function UserQuiz({ userQuiz, setStep }) {
+  const [quizValues, setQuizValues] = React.useState({});
 
-function UserQuiz(){
+  function submitQuiz() {
+    console.log('Submitted Quiz Values:', quizValues);
+    alert('Quiz submitted successfully!');
+  }
 
+  function handleValues(e) {
+    const temp = { [e.target.name]: e.target.value };
+    setQuizValues((prev) => ({ ...prev, ...temp }));
+  }
 
+  return (
+    <section className="p-5">
+      <section className="border border-dark p-5 m-2 rounded-3">
+        <div className="border border-dark rounded-3 p-5 m-5 text-center">
+          <h3>{userQuiz?.quizName || 'Quiz Name'}</h3>
+          <div className="m-4 d-flex justify-content-around">
+            <h5>{userQuiz?.quizFormDescription || 'Description not available'}</h5>
+            <h5>No of Questions: {userQuiz?.quizNoOfQuestions || 0}</h5>
+          </div>
+        </div>
 
-     return (
-        <section>
-             <h1>Hi</h1>
-        </section>
-     )
-       
-    
+        {userQuiz?.quizQuestions?.length > 0 ? (
+          userQuiz.quizQuestions.map((el, ind) => (
+            <div
+              className="text-start m-5 border border-dark rounded-3 p-5"
+              key={`${el.questionName.slice(0, 10)}-${ind}`}
+            >
+              <p>{el.questionName}</p>
+              <ul type="none">
+                {el?.options?.map((option, idx) => (
+                  <li className="d-flex gap-3" key={`${option.option}-${idx}`}>
+                    <input
+                      type={el.questionType}
+                      name={el.questionName}
+                      value={option.option}
+                      onChange={handleValues}
+                    />
+                    <span>{option.option}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))
+        ) : (
+          <p className="text-center">No questions available for this quiz.</p>
+        )}
+
+        <div className="p-5 d-flex justify-content-between">
+          <button
+            onClick={() => {
+              setStep(1);
+            }}
+            className="btn btn-secondary"
+          >
+            Previous
+          </button>
+
+          <button onClick={submitQuiz} className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </section>
+    </section>
+  );
 }
 
-export default UserQuiz
+export default UserQuiz;
